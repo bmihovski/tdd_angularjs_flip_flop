3c60a817e49ec947d90e593d4d0f85d6a7b9b5a4
Gogs <gogs@fake.local>
2018-12-13 17:22:48 +0200

    tests passed successfully

--- spec/e2e/scenario.js
+++ spec/e2e/scenario.js
@@ -2,10 +2,10 @@ describe('Given a view A that has a single button', function() {
 	describe('When the button is pushed', function() {
 		var viewA = element(by.css('#viewA'));
 		var viewB = element(by.css('#viewB'));
+		var buttonToPush = element(by.linkText('flip'));
 		beforeEach(function() {
 			browser.get('/');
 			expect(viewA.isPresent()).toBeTruthy();
-			var buttonToPush = element(by.linkText('flip'));
 			buttonToPush.click();
 		});
 		it('Should be switched to view B', function() {
@@ -13,6 +13,10 @@ describe('Given a view A that has a single button', function() {
 		});
 		it('Should not display view A', function() {
 			expect(viewA.isPresent()).toBeFalsy();
-		})
+		});
+		it('Should be switched to view A', function() {
+			buttonToPush.click();
+			expect(viewA.isPresent()).toBeTruthy();
+		});
 	});
 });
